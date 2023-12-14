b7f44c0b3b951f3a4f6499ffc0f5a0f6b1979cfc
Kevin Klions <kklions1@gmail.com>
2023-12-13 22:48:59 -0500

    COLORSgit add -A git add -A git add -A git add -A !

--- src/gui/gui_main.cpp
+++ src/gui/gui_main.cpp
@@ -4,7 +4,7 @@
 
 sf::RectangleShape light_square(float x, float y) { 
     sf::RectangleShape square(sf::Vector2f(100.0f, 100.0f));
-    square.setFillColor(sf::Color::White);
+    square.setFillColor(sf::Color(0, 138, 209));
     square.setPosition(sf::Vector2f(x, y));
     
     // std::cout << "light square at (" << x << ", " << y << ")\n";
@@ -14,7 +14,7 @@ sf::RectangleShape light_square(float x, float y) {
 
 sf::RectangleShape dark_square(float x, float y) { 
     sf::RectangleShape square(sf::Vector2f(100.0f, 100.0f));
-    square.setFillColor(sf::Color::Black);
+    square.setFillColor(sf::Color(0, 4, 71));
     square.setPosition(sf::Vector2f(x, y));
     
     // std::cout << "dark square at (" << x << ", " << y << ")\n";
@@ -25,8 +25,8 @@ sf::RectangleShape dark_square(float x, float y) {
 std::vector<sf::RectangleShape> generate_squares() { 
     float current_x = 0.0f;
     float current_y = 0.0f;
-    std::vector<sf::RectangleShape> squares; 
     bool color_alternator = true;
+    std::vector<sf::RectangleShape> squares; 
 
     for (int rank = 0; rank < 8; rank++) { 
         for (int file = 0; file < 8; file++) { 
--- src/main.cpp
+++ src/main.cpp
@@ -10,15 +10,13 @@ int main() {
     std::string test_fen = "rn1q1rk1/pp2ppbp/3p1np1/2p2b2/3P1B2/1QP1PN2/PP1NBPPP/R3K2R b KQ - 1 8"; 
     std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
-    // auto game_board = std::make_unique<Board>();
+    auto game_board = std::make_unique<Board>();
+    parse_fen_string(test_fen, *game_board);
+    print_board(*game_board);
 
-    // parse_fen_string(test_fen, *game_board);
-    // print_board(*game_board);
-    // // std::string generated_fen = generate_fen_string(*game_board);
-
-    // auto starting_position = std::make_unique<Board>();
-    // parse_fen_string(starting_fen, *starting_position);
-    // print_board(*starting_position);
+    auto starting_position = std::make_unique<Board>();
+    parse_fen_string(starting_fen, *starting_position);
+    print_board(*starting_position);
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "Chess");
 
@@ -34,8 +32,8 @@ int main() {
 
         window.clear(sf::Color::Green); // If green is showing, its probably because of some rendering problem
 
-        for (sf::RectangleShape r : squares) { 
-            window.draw(r);
+        for (sf::RectangleShape s : squares) { 
+            window.draw(s);
         }
 
         window.display();
