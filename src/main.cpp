d38096228ae93a7a949a7a090ee0fec37c3148d4
Kevin Klions <kklions1@gmail.com>
2023-12-14 12:11:13 -0500

    the pieces are there but in the wrong orientation

--- src/main.cpp
+++ src/main.cpp
@@ -8,21 +8,25 @@
 
 int main() { 
     // std::string test_fen = "rn1q1rk1/pp2ppbp/3p1np1/2p2b2/3P1B2/1QP1PN2/PP1NBPPP/R3K2R b KQ - 1 8"; 
-    // std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+    std::string starting_fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
 
     // auto game_board = std::make_unique<Board>();
     // parse_fen_string(test_fen, *game_board);
     // print_board(*game_board);
 
-    // auto starting_position = std::make_unique<Board>();
-    // parse_fen_string(starting_fen, *starting_position);
-    // print_board(*starting_position);
+    auto starting_position = std::make_unique<Board>();
+    parse_fen_string(starting_fen, *starting_position);
 
     sf::RenderWindow window(sf::VideoMode(800, 800), "Chess");
 
     auto texture_map = init_textures();
     auto squares = generate_squares();
-    
+
+    for (int i = 0; i < 64; ++i) { 
+        int piece = starting_position->board[i];
+        squares[i].setTexture(texture_map[piece].get());
+    }
+
     while (window.isOpen()) { 
         sf::Event event;
         while (window.pollEvent(event)) { 
