fd87de52880920fa8b25981f8d4129035f09e439
Kevin Klions <kklions1@gmail.com>
2024-01-10 14:45:22 -0500

    fixed literal edge cases where pawns could 'see' the other side of the board when they were not supposed to

--- src/common/piece.cpp
+++ src/common/piece.cpp
@@ -3,6 +3,14 @@
 int color_mask = 0b11000;
 int piece_mask = 0b00111;
 
+std::set<int> edges = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 15, 16, 23, 24,
+     31, 32, 39, 40, 47, 48, 55, 56, 57, 58, 59, 60, 61, 62, 63 }; 
+
+std::set<int> left_edges = { 0, 8, 16, 24, 32, 40, 48, 56 };
+std::set<int> right_edges = { 7, 15, 23, 31, 39, 47, 55, 63 };
+std::set<int> top_edges = { 0, 1, 2, 3, 4, 5, 6, 7 };
+std::set<int> bottom_edges = { 56, 57, 58, 59, 60, 61, 62, 63 };
+
 int Piece::color() { 
     return color_mask & this->data;
 }
@@ -15,13 +23,32 @@ void calculate_pawn_vision(Piece& self, int index) {
     int left, right, advance; 
 
     if (self.color() == PieceType::WHITE) { 
-        self.vision.push_back(index - 9); // left  
-        self.vision.push_back(index - 7); // right 
-        self.vision.push_back(index - 8); // forward
+        left = index - 9;
+        right = index - 7;
+        advance = index - 8;
+        // self.vision.push_back(index - 9); // left  
+        // self.vision.push_back(index - 7); // right 
+        // self.vision.push_back(index - 8); // forward
     } else { 
-        self.vision.push_back(index + 9); 
-        self.vision.push_back(index + 7);
-        self.vision.push_back(index + 8);
+        right = index + 9;
+        left = index + 7;
+        advance = index + 8;
+
+        // self.vision.push_back(index + 9); 
+        // self.vision.push_back(index + 7);
+        // self.vision.push_back(index + 8);
     }
+
+
+    if (!left_edges.contains(advance)) { 
+        self.vision.push_back(left);
+    }
+
+    if (!right_edges.contains(advance)) { 
+        self.vision.push_back(right);
+    }
+
+    self.vision.push_back(advance);
+
 }
 
--- src/common/piece.hpp
+++ src/common/piece.hpp
@@ -1,9 +1,15 @@
 #pragma once 
 
 #include <vector>
+#include <set>
 
 extern int piece_mask;
 extern int color_mask;
+
+extern std::set<int> left_edges; // A File 
+extern std::set<int> right_edges; // H File 
+extern std::set<int> top_edges; // 8th Rank
+extern std::set<int> bottom_edges; // 1st Rank
 
 typedef enum PieceType { 
     EMPTY = 0, 
@@ -29,6 +35,8 @@ typedef struct Piece {
 
     Piece(): data(PieceType::EMPTY), vision(std::vector<int>()), calc_vision([](Piece& self, int index) { /* no-op */ }) {}
 } Piece; 
+
+
 
 void calculate_pawn_vision(Piece&, int);
 
